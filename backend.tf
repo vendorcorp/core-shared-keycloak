2c9673979114db7add0c9daca5037fbd6a0cf103
bcbarbosa <bbarbosa@sonatype.com>
2023-04-24 15:08:55 +0100

    Revert required providers

--- backend.tf
+++ backend.tf
@@ -5,18 +5,4 @@ terraform {
     dynamodb_table = "vendorcorp-terraform-state-lock"
     region         = "us-east-2"
   }
-  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = ">= 4.6.0"
-#    }
-#    kubernetes = {
-#      source  = "hashicorp/kubernetes"
-#      version = ">= 2.19.0"
-#    }
-    postgresql = {
-      source  = "cyrilgdn/postgresql"
-      version = ">= 1.15.0"
-    }
-  }
 }
